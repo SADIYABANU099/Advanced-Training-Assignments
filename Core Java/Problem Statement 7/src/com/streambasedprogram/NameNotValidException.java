9ffdc1b508b5e4cb4dbda51f5bd5e19d5ff763e6
SadiyaBanu <Sadiyabanu099@gmail.com>
2022-04-28 17:37:12 +0530

    advance training 1-8 project

---
+++ Core Java/Problem Statement 7/src/com/streambasedprogram/NameNotValidException.java
@@ -0,0 +1,10 @@
+package com.streambasedprogram;
+
+
+class Demo extends Exception
+{
+     public String validname()
+     {
+          return ("Name is not Valid..Please ReEnter the Name");
+     }
+}
